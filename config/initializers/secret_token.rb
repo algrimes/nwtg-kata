5bfbba55c994fbd0d484e656ecb169572d8f7578
algrimes <agrimes@thoughtworks.com>
2012-07-24 18:19:53 +0100

    Al : Adding the rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NwtgKata::Application.config.secret_token = '7b2c3872ba51302b5de97567e6352d424885d77dff746135b63ba415c4ea70e66040631fd016736bb212504285ee48d95b64dd7dc8c2eb15bd5256d8cd488912'
